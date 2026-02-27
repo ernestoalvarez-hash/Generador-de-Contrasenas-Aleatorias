c4d21b1b4670da72bc55f89777261d6e25fafdf5
Ernesto Alvarez <ernesto.alvarez@mi.unc.edu.ar>
2026-02-27 19:00:21 -0300

    Agregado la seleccion de configuracion para la contraseña y el punto de entrada principal

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-#TO DO: hacer el docstring y el resto de las opciones
+#TO DO: hacer el docstring
 def pass_conf():
-    charateres = input("¿Quiere que la contraseña tenga caracteres? : ")
+    strings = str(input("¿Quiere que la contraseña tenga letras?SI/NO: ")).lower()
+    nums = str(input("¿Quieres que la contraseña tenga numeros?SI/NO: ")).lower()
+    spec_char = str(input("¿Queres que la contraseña tenga caracteres especiales?SI/NO: ")).lower()
+    config = {"strings": strings,
+              "nums": nums,
+              "spec_char": spec_char}
+    return config
     
 #TO DO: hacer el docstring
 def main():
     confg = pass_conf() 
+
+if __name__ == "__main__":
+    main()
