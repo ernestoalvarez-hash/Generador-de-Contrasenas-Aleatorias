644a6102f3fc60d6195accdafa4fbf82ab51f53e
Ernesto Alvarez <ernesto.alvarez@mi.unc.edu.ar>
2026-02-27 18:43:26 -0300

    Subida del archivo principal con las funciones main y pass_conf

---
+++ main.py
@@ -0,0 +1,7 @@
+#TO DO: hacer el docstring y el resto de las opciones
+def pass_conf():
+    charateres = input("¿Quiere que la contraseña tenga caracteres? : ")
+    
+#TO DO: hacer el docstring
+def main():
+    confg = pass_conf() 
