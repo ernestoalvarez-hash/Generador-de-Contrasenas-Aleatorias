23077893b3348aa34856ea1233388e7b6cb0a957
Ernesto Alvarez <ernesto.alvarez@mi.unc.edu.ar>
2026-03-03 22:12:00 -0300

    Añadido checqueos para la configuracion de la contraseña

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import random
 import string
 
+def check_conf(config: list):
+    if "si" not in config:
+        raise ValueError("Debe elegir al menos una opcion con 'si'")
+    
+
 #TO DO: hacer el docstring
 def pass_conf():
     lower_case = str(input("¿Quiere que la contraseña tenga letras minusculas?SI/NO: ")).lower()
     upper_case = str(input("¿Quiere que la contraseña tenga letras mayusculas?SI/NO: ")).lower()
     nums = str(input("¿Quieres que la contraseña tenga numeros?SI/NO: ")).lower()
     spec_char = str(input("¿Queres que la contraseña tenga caracteres especiales?SI/NO: ")).lower()
+    
+    options = [lower_case,upper_case,nums,spec_char]
+    check_conf(options)
+    
     config = {"lower_case": lower_case,
               "upper_case": upper_case,
               "nums": nums,
@@ -28,9 +37,12 @@ def generate_alfhabet(conf:dict):
     
 #TO DO: hacer el docstring
 def main():
-    confg = pass_conf()
-    alfhabet = generate_alfhabet(confg)
+    try:
+        config = pass_conf()
+        alfhabet = generate_alfhabet(config)
 
+    except ValueError as e:
+        print("Error:", e)
 
 
 if __name__ == "__main__":
