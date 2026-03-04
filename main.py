2e2bb88b3a130e37bdcf0bdc8bf316c728feb013
Ernesto Alvarez <ernesto.alvarez@mi.unc.edu.ar>
2026-03-03 22:25:00 -0300

    Añadido seleccion y chequeo de longitud de contraseña

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 import string
 
-def check_conf(config: list):
+#TO DO: hacer el docstring
+def check_conf(config: list,size_password: int):
     if "si" not in config:
         raise ValueError("Debe elegir al menos una opcion con 'si'")
-    
+    if  0 > size_password or size_password > 24:
+        raise ValueError("La contraseña debe tener menos de 24 caracteres y mas de 1")      
 
 #TO DO: hacer el docstring
 def pass_conf():
@@ -12,14 +14,16 @@ def pass_conf():
     upper_case = str(input("¿Quiere que la contraseña tenga letras mayusculas?SI/NO: ")).lower()
     nums = str(input("¿Quieres que la contraseña tenga numeros?SI/NO: ")).lower()
     spec_char = str(input("¿Queres que la contraseña tenga caracteres especiales?SI/NO: ")).lower()
+    size_password = int(input("Ingrese la longitud de la contraseña: "))
     
     options = [lower_case,upper_case,nums,spec_char]
-    check_conf(options)
+    check_conf(options,size_password)
     
     config = {"lower_case": lower_case,
               "upper_case": upper_case,
               "nums": nums,
-              "spec_char": spec_char}
+              "spec_char": spec_char,
+              "size_password": size_password}
     return config
 
 #TO DO: hacer el docstring
